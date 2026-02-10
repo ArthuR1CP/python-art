94a49fbfdff5084ac0141ddc0fa7ec82da46376e
Artur Cepuc <arturcepuc@gmail.com>
2026-02-10 20:08:51 +0200

    Fail test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,3 +3,9 @@ from utils import calculate_total
 def test_calculate_total():
     assert calculate_total([10, 15, 25]) == 50
     assert calculate_total([]) == 0
+
+
+
+def test_calculate_total():
+    assert calculate_total([10, 15, 25]) == 55
+    assert calculate_total([]) == 0
